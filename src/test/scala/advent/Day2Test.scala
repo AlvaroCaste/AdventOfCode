f503998892c7f12b9d011f52a13392be89fd44ba
Álvaro Castellanos <alvaro.castellanos.lopez@gmail.com>
2016-06-19 21:56:12 +0200

    Solve Day 2

--- src/test/scala/advent/Day2Test.scala
+++ src/test/scala/advent/Day2Test.scala
@@ -4,21 +4,25 @@ import org.scalatest.{FlatSpec, ShouldMatchers}
 
 class Day2Test extends FlatSpec with ShouldMatchers {
 
+  private val example1 = "2x3x4"
+  private val example2 = "1x1x10"
+  private val example3 = example1 + "\n" + example2 + "\n"
+
   "Part 1 wrapping paper" should "compute the wrapping paper" in {
-    Day2.part1("2x3x4") shouldBe 58
-    Day2.part1("1x1x10") shouldBe 43
+    Day2.part1(example1) shouldBe 58
+    Day2.part1(example2) shouldBe 43
   }
 
   it should "add up the area for all presents" in {
-    Day2.part1("2x3x4\n1x1x10\n") shouldBe 58 + 43
+    Day2.part1(example3) shouldBe 58 + 43
   }
 
   "Part 2 wrapping paper" should "compute the ribbon length" in {
-    Day2.part2("2x3x4") shouldBe 34
-    Day2.part2("1x1x10") shouldBe 14
+    Day2.part2(example1) shouldBe 34
+    Day2.part2(example2) shouldBe 14
   }
 
   it should "add up the length for all presents" in {
-    Day2.part2("2x3x4\n1x1x10\n") shouldBe 34 + 14
+    Day2.part2(example3) shouldBe 34 + 14
   }
 }
